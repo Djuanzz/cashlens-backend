a0a9c9f1540fe6b48231cc1e77e31686bff95165
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-03-17 01:01:06 +0800

    feat(repository): implement health repository

--- internal/repository/health_repository.go
+++ internal/repository/health_repository.go
@@ -1,1 +1,21 @@
 package repository
+
+import "github.com/Djuanzz/cashlens-backend/internal/model"
+
+type HealthRepository struct {
+}
+
+// --- constructor function untuk membuat instance baru dari HealthRepository
+// --- retrun pointer karena nanti akan di assign ke struct handler
+// ---biar ga terjadi copy paste struct handler nya
+func NewHealthRepository() *HealthRepository {
+	// --- return & karena return type nya pointer
+	return &HealthRepository{}
+}
+
+// --- method receiver artinya method milik struct HealthRepository
+func (r *HealthRepository) GetHealth() model.Health {
+	return model.Health{
+		Status: "OK",
+	}
+}
