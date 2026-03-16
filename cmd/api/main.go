16eeaea2c3542b85a2ebed5e79f7ac7065926e64
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-03-17 01:03:23 +0800

    feat(api): initialize http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Djuanzz/cashlens-backend/internal/router"
+)
 
 func main() {
 	fmt.Println("===== Cashlens Backend =====")
+
+	r := router.SetupRouter()
+	r.Run(":5000")
 }
