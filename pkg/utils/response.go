e2424c4473ac464b8ecc261552c11cd5781bc905
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-03-17 01:03:12 +0800

    feat(utils): add response helper

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -1,1 +1,23 @@
 package utils
+
+import "github.com/gin-gonic/gin"
+
+type Response struct {
+	Status  string      `json:"status" form:"status"`
+	Message string      `json:"message" form:"message" omitempty:""`
+	Data    interface{} `json:"data" form:"data" omitempty:""`
+}
+
+func SuccessResponse(c *gin.Context, httpStatus int, data interface{}) {
+	c.JSON(httpStatus, Response{
+		Status: "success",
+		Data:   data,
+	})
+}
+
+func ErrorResponse(c *gin.Context, httpStatus int, message string) {
+	c.JSON(httpStatus, Response{
+		Status:  "error",
+		Message: message,
+	})
+}
