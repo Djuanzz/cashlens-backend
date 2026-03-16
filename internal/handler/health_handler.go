e5247d49d4ee16f055a25914af38c4093421190f
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-03-17 01:01:21 +0800

    feat(handler): add health handler

--- internal/handler/health_handler.go
+++ internal/handler/health_handler.go
@@ -1,1 +1,29 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/Djuanzz/cashlens-backend/internal/repository"
+	"github.com/Djuanzz/cashlens-backend/internal/service"
+	"github.com/Djuanzz/cashlens-backend/pkg/utils"
+	"github.com/gin-gonic/gin"
+)
+
+type HealthHandler struct {
+	service *service.HealthService
+}
+
+func NewHealthHandler() *HealthHandler {
+	repo := repository.NewHealthRepository()
+	service := service.NewHealthService(repo)
+
+	return &HealthHandler{
+		service: service,
+	}
+}
+
+func (h *HealthHandler) CheckHealth(C *gin.Context) {
+	result := h.service.CheckHealth()
+
+	utils.SuccessResponse(C, http.StatusOK, result)
+}
